6cb97430ac9574766cb81b609e8735eb9a124228
Ricardo Lopes <ricardorpl@gmail.com>
2011-05-15 11:05:10 +0100

    .

--- codigo/y.tab.h
+++ codigo/y.tab.h
@@ -1,118 +1,34 @@
-
-/* A Bison parser, made by GNU Bison 2.4.1.  */
-
-/* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
-   
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
-
-/* As a special exception, you may create a larger work that contains
-   part or all of the Bison parser skeleton and distribute that work
-   under terms of your choice, so long as that work isn't itself a
-   parser generator using the skeleton or a modified version thereof
-   as a parser skeleton.  Alternatively, if you modify or redistribute
-   the parser skeleton itself, you may (at your option) remove this
-   special exception, which will cause the skeleton and the resulting
-   Bison output files to be licensed under the GNU General Public
-   License without this special exception.
-   
-   This special exception was added by the Free Software Foundation in
-   version 2.2 of Bison.  */
-
-
-/* Tokens.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     CLASS = 258,
-     STATIC = 259,
-     INT = 260,
-     STRING = 261,
-     VOID = 262,
-     FLOAT = 263,
-     DOUBLE = 264,
-     BOOLEAN = 265,
-     CHAR = 266,
-     IF = 267,
-     WHILE = 268,
-     FOR = 269,
-     DO = 270,
-     SWITCH = 271,
-     CASE = 272,
-     PRINT = 273,
-     PRINTLN = 274,
-     TRUE = 275,
-     FALSE = 276,
-     EQUALS = 277,
-     DIFERENT = 278,
-     GREATER = 279,
-     LESSER = 280,
-     GREATEQ = 281,
-     LESSEQ = 282,
-     AND = 283,
-     OR = 284,
-     VAR = 285,
-     NUMBER = 286,
-     UMINUS = 287
-   };
-#endif
-/* Tokens.  */
-#define CLASS 258
-#define STATIC 259
-#define INT 260
-#define STRING 261
-#define VOID 262
-#define FLOAT 263
-#define DOUBLE 264
-#define BOOLEAN 265
-#define CHAR 266
-#define IF 267
-#define WHILE 268
-#define FOR 269
-#define DO 270
-#define SWITCH 271
-#define CASE 272
-#define PRINT 273
-#define PRINTLN 274
-#define TRUE 275
-#define FALSE 276
-#define EQUALS 277
-#define DIFERENT 278
-#define GREATER 279
-#define LESSER 280
-#define GREATEQ 281
-#define LESSEQ 282
-#define AND 283
-#define OR 284
-#define VAR 285
-#define NUMBER 286
-#define UMINUS 287
-
-
-
-
-#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-{
-
-/* Line 1676 of yacc.c  */
-#line 11 "pjava.y"
-
+#define CLASS 257
+#define STATIC 258
+#define INT 259
+#define STRING 260
+#define VOID 261
+#define FLOAT 262
+#define DOUBLE 263
+#define BOOLEAN 264
+#define CHAR 265
+#define IF 266
+#define WHILE 267
+#define FOR 268
+#define DO 269
+#define SWITCH 270
+#define CASE 271
+#define PRINT 272
+#define PRINTLN 273
+#define TRUE 274
+#define FALSE 275
+#define EQUALS 276
+#define DIFERENT 277
+#define GREATER 278
+#define LESSER 279
+#define GREATEQ 280
+#define LESSEQ 281
+#define AND 282
+#define OR 283
+#define VAR 284
+#define NUMBER 285
+#define UMINUS 286
+typedef union{
 	is_static_list* isl;
 	is_atribuicao_list* ial;
 	is_atributo* ia;
@@ -133,17 +49,5 @@ typedef union YYSTYPE
 	char* var;
 	is_if* ii;
 	is_for* isf;
-
-
-
-/* Line 1676 of yacc.c  */
-#line 141 "y.tab.h"
 } YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
-#endif
-
 extern YYSTYPE yylval;
-
-
